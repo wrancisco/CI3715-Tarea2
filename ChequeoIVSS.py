668ca0178680c71f5f34ebf16af06cdaba93d20a
wrancisco <opefran@gmail.com>
2018-10-10 16:58:53 -0400

    Comentarios

--- ChequeoIVSS.py
+++ ChequeoIVSS.py
@@ -4,16 +4,20 @@
 
 from datetime import datetime, date
 
+# Calcula la edad de una persona a partit de su fecha de nacimiento.
 def calculador_de_edad(nacimiento):
     today = date.today()
     return today.year - nacimiento.year - (((today.month, today.day) < (nacimiento.month, nacimiento.day)))
 
+# Verifica el genero de una persona, para el valor de masculino devulve ´verdadero´ y ´falso´ para femenino.
 def chequeo_de_genero(letra_de_genero):
     return letra_de_genero == "M" or letra_de_genero == "m"
 
+# Verifica que la cantidad de horas cotizadas sea mayor o igual a 750 horas, de lo contrario retorna falso
 def chequeo_de_semanas(weeks):
     return weeks >= 750
 
+# Calcula la cantidad de anos que se decuentas por trabajos realizados en condiciones isalubres
 def descuento(anos_insalubres):
     return min(anos_insalubres // 4, 5)
     
